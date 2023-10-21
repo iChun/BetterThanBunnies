eb58001535aa40d09b2950ca15126f6aaba8573d
iChun <ohai.iChun@gmail.com>
2023-10-21 03:24:06 +0100

    Update to 1.20.2

--- src/main/java/me/ichun/mods/betterthanbunnies/loader/forge/LoaderForge.java
+++ src/main/java/me/ichun/mods/betterthanbunnies/loader/forge/LoaderForge.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.network.NetworkConstants;
+
 
 @Mod(BetterThanBunnies.MOD_ID)
 public class LoaderForge extends BetterThanBunnies
@@ -24,7 +24,7 @@ public class LoaderForge extends BetterThanBunnies
         DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 
     private void setupConfig()
