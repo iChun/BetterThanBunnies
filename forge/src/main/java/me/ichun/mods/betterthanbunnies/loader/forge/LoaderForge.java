3c1cbc43dd5bd671ebb8bc69cea4590a099d423c
iChun <ohai.iChun@gmail.com>
2024-06-24 18:22:00 +0100

    Bump version in gradle.properties. Fixes for loading on Forge and NeoForge. WE'RE ALMOST THERE!

--- forge/src/main/java/me/ichun/mods/betterthanbunnies/loader/forge/LoaderForge.java
+++ forge/src/main/java/me/ichun/mods/betterthanbunnies/loader/forge/LoaderForge.java
@@ -2,9 +2,11 @@ package me.ichun.mods.betterthanbunnies.loader.forge;
 
 import me.ichun.mods.betterthanbunnies.common.BetterThanBunnies;
 import me.ichun.mods.betterthanbunnies.common.core.Config;
+import me.ichun.mods.ichunutil.client.gui.config.WorkspaceConfigs;
 import me.ichun.mods.ichunutil.common.iChunUtil;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -17,10 +19,7 @@ public class LoaderForge extends BetterThanBunnies
     {
         modProxy = this;
 
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-            setupConfig();
-            new EventHandlerClientForge();
-        });
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::initClient);
         DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
@@ -28,9 +27,13 @@ public class LoaderForge extends BetterThanBunnies
     }
 
     @OnlyIn(Dist.CLIENT)
-    private void setupConfig()
+    private void initClient()
     {
         config = iChunUtil.d().registerConfig(new Config());
+
+        new EventHandlerClientForge();
+
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(WorkspaceConfigs::new));
     }
 
 }
--- neoforge/src/main/java/me/ichun/mods/betterthanbunnies/loader/neoforge/LoaderNeoForge.java
+++ neoforge/src/main/java/me/ichun/mods/betterthanbunnies/loader/neoforge/LoaderNeoForge.java
@@ -2,12 +2,15 @@ package me.ichun.mods.betterthanbunnies.loader.neoforge;
 
 import me.ichun.mods.betterthanbunnies.common.BetterThanBunnies;
 import me.ichun.mods.betterthanbunnies.common.core.Config;
+import me.ichun.mods.ichunutil.client.gui.config.WorkspaceConfigs;
 import me.ichun.mods.ichunutil.common.iChunUtil;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(BetterThanBunnies.MOD_ID)
 public class LoaderNeoForge extends BetterThanBunnies
@@ -33,5 +36,7 @@ public class LoaderNeoForge extends BetterThanBunnies
         config = iChunUtil.d().registerConfig(new Config(), modEventBus);
 
         new EventHandlerClientNeoForge(modEventBus);
+
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (minecraft, screen) -> new WorkspaceConfigs(screen));
     }
 }
