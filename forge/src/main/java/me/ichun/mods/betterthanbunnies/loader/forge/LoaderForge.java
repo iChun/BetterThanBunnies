1fb097047b42858e1c8c8ba00c18b4ed28c88a91
iChun <ohai.iChun@gmail.com>
2024-06-26 18:40:14 +0100

    Backport 1.19.2 to 1.18.2. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed. RIP some of iChunUtil's ported code as this commit removes them as they weren't ported to 1.20.6.

---
+++ forge/src/main/java/me/ichun/mods/betterthanbunnies/loader/forge/LoaderForge.java
@@ -0,0 +1,39 @@
+package me.ichun.mods.betterthanbunnies.loader.forge;
+
+import me.ichun.mods.betterthanbunnies.common.BetterThanBunnies;
+import me.ichun.mods.betterthanbunnies.common.core.Config;
+import me.ichun.mods.ichunutil.client.gui.config.WorkspaceConfigs;
+import me.ichun.mods.ichunutil.common.iChunUtil;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BetterThanBunnies.MOD_ID)
+public class LoaderForge extends BetterThanBunnies
+{
+    public LoaderForge()
+    {
+        modProxy = this;
+
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::initClient);
+        DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
+
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (remoteVersion, isFromServer) -> true));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private void initClient()
+    {
+        config = iChunUtil.d().registerConfig(new Config());
+
+        new EventHandlerClientForge();
+
+        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((mc, screen) -> new WorkspaceConfigs(screen)));
+    }
+
+}
