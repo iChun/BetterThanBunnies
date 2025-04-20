7c8e161957f708e0e310aa4b6ff6ce02992a0afd
iChun <ohai.iChun@gmail.com>
2025-04-20 19:43:18 +0100

    Initial port to 1.21.5. Not yet tested. Update shadow plugin. Revert to NeoGradle from ModDevKit.

--- common/src/main/java/me/ichun/mods/betterthanbunnies/client/render/BunnyFancyLayer.java
+++ common/src/main/java/me/ichun/mods/betterthanbunnies/client/render/BunnyFancyLayer.java
@@ -16,7 +16,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.animal.Rabbit;
-import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.animal.sheep.Sheep;
 import net.minecraft.world.item.DyeColor;
 
 import java.util.Random;
